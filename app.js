15482dfffa246a2ecdd403f6c42138dd10c5de32
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-03-25 15:15:07 +0500

    tour adding opportunity has been added

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
+// importing packages
 const express = require("express");
 const app = express();
 const fs = require("fs");
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({message: "Hello from the server side!", app: "Natours"});
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint...");
-// })
+// calling middlewares
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "UTF-8"));
 
 
-app.get('/api/v1/tours', (req, res) => {
-  // fs.readFile(`${__dirname}/dev-data/data/tours-simple.json`, "UTF-8", (err, data) => {
-  //   res.status(200).json(JSON.parse(data));
-  // });
-  // const readible = fs.createReadStream(`${__dirname}/dev-data/data/tours-simple.json`, "UTF-8");
-  // readible.pipe(res);
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
@@ -26,9 +17,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
-
-  
 });
+
+app.post("/api/v1/tours", (req, res) => {
+  const newTourId = tours[tours.length-1].id + 1;
+  const newTour = {id: newTourId, ...req.body};
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if(err) console.log("Some error was occured when writeing data to file, pleace try to rewrite data");
+    else {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
