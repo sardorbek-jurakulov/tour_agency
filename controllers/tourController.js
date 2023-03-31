89e0676338a88ff1f58fb9ee1c4e6dd6173a0fed
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-03-31 12:39:53 +0500

    Controllers are successful created and tested

---
+++ controllers/tourController.js
@@ -0,0 +1,124 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next();
+});
+
+const DB_PATH = `${__dirname}/../dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(DB_PATH, "UTF-8"));
+
+// route handlers
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+}; 
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  if(!tour) {
+    res.status(404).json({
+      "status": "not founded",
+      "data": {
+      }
+    });
+  } else {
+    res.status(200).json({
+      "status": "success",
+      "data": {
+        "tour": tour
+      }
+    });
+  }
+};
+
+exports.createTour = (req, res) => {
+  const newTourId = tours[tours.length-1].id + 1;
+  const newTour = {id: newTourId, ...req.body};
+  tours.push(newTour);
+  fs.writeFile(DB_PATH, JSON.stringify(tours), err => {
+    if(err) { 
+      res.status(500).json({
+        status: "Internal Server Error",
+        message: "Some error was occured when writeing data to the database, pleace try to rewrite data",
+        data: {}
+      });
+    }
+    else {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  });
+};
+
+exports.updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const updatingTourIndex = tours.findIndex(tour => tour.id === id);
+  if(updatingTourIndex === -1) {
+    res.status(404).json({
+      "status": "not founded",
+      "data": {}
+    });
+  } else {
+    const updatedTour = {id, ...req.body}
+    tours.splice(updatingTourIndex, 1, updatedTour);
+    fs.writeFile(DB_PATH, JSON.stringify(tours), err => {
+      if(err) {
+        res.status(500).json({
+          status: "Internal Server Error",
+          message: "Some error was occured when writeing data to the database, pleace try to update data",
+          data: {},
+        });
+      } else {
+        res.status(200).json({
+          "status": "success",
+          "data": {
+            "tour": updatedTour
+          }
+        });
+      }
+    })
+  }
+};
+
+exports.deleteTour = (req, res) => {
+  const deletingTourIndex = tours.findIndex(tour => tour.id === (req.params.id * 1));
+
+  if(deletingTourIndex === -1) {
+    res.status(404).json({
+      "status": "not founded",
+      "data": {}
+    });
+  } else {
+    tours.splice(deletingTourIndex, 1);
+    fs.writeFile(DB_PATH, JSON.stringify(tours), err => {
+      if(err) { 
+        res.status(500).json({
+          status: "Internal Server Error",
+          message: "Some error was occured when writeing data to the database, pleace try to delete data",
+          data: {}
+        });
+      } else {
+        res.status(204).json({
+          status: "No Content",
+          data: null
+        });
+      }
+    })
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,64 +1,17 @@
 const express = require("express");
-const fs = require("fs");
+const userController = require("../controllers/userController.js");
 
-const app = express();
 const router = express.Router();
-
-app.use(express.json());
-
-const DB_PATH = `${__dirname}/../dev-data/data/tours-simple.json`;
-const tours = JSON.parse(fs.readFileSync(DB_PATH, "UTF-8"));
-
-// route handlers
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
-  });
-};
 
 router
   .route("/")
-  .get(getAllUsers)
-  .post(createUser);
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
   .route("/:id")
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
