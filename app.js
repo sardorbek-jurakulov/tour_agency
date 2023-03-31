9fd306bada6d85a87783a3a36c77662a02625cc6
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-03-31 16:42:35 +0500

    WIP

--- app.js
+++ app.js
@@ -10,7 +10,4 @@ app.use(morgan("dev"));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+module.exports = app;
