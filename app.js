dfbdbfed386d390d2b62c2959aa4680ea55a311c
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-02 07:16:44 +0500

    static file recognizer middleware has been added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   next();
 });
+app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
