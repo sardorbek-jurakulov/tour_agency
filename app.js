3f635ec7d019ec200cbe8f8299b6811dd407524a
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-02 12:14:12 +0500

    environment variables has been added

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userRouter = require("./routes/userRoutes.js");
 const app = express();
 
 // calling middlewares
-app.use(morgan("dev"));
+if(process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app.js");
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
