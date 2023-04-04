149a9b592dabe562c69d88d21c4a28dfc3b93f23
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-04 14:37:51 +0500

    code has been refactored for requirement eslint and prettier

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,46 @@
-const fs = require("fs");
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 app.use(express.json());
 
-const DB_PATH = `${__dirname}/../dev-data/data/tours-simple.json`;
-const tours = JSON.parse(fs.readFileSync(DB_PATH, "UTF-8"));
-
 // route handlers
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
+    status: 'Internel server error',
+    message: 'This route is not yet defined!',
+    data: {},
   });
 };
 
 exports.createUser = (req, res) => {
   res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
+    status: 'Internel server error',
+    message: 'This route is not yet defined!',
+    data: {},
   });
 };
 
 exports.getUser = (req, res) => {
   res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
+    status: 'Internel server error',
+    message: 'This route is not yet defined!',
+    data: {},
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
+    status: 'Internel server error',
+    message: 'This route is not yet defined!',
+    data: {},
   });
 };
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
-    status: "Internel server error",
-    message: "This route is not yet defined!",
-    data: {}
+    status: 'Internel server error',
+    message: 'This route is not yet defined!',
+    data: {},
   });
 };
