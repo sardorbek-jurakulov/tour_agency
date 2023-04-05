cf973b1f49362c8ac0a017da2abee35cb87b6e57
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-05 10:29:00 +0500

    switched from file storing to real mongoDB cloud

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-
-const DB_PATH = `${__dirname}/../dev-data/data/tours-simple.json`;
-const tours = JSON.parse(fs.readFileSync(DB_PATH, 'UTF-8'));
-
-exports.checkID = (req, res, next, val) => {
-  const requestedTourIndex = tours.findIndex(
-    (el) => el.id === req.params.id * 1
-  );
-  if (requestedTourIndex === -1) {
-    return res.status(404).json({
-      status: 'not founded',
-      message: `No element found with id ${val} in tours`,
-      data: {},
-    });
-  }
-  req.requestedTourIndex = requestedTourIndex;
-  next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -34,9 +16,9 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
+    // results: tours.length,
     data: {
-      tours: tours,
+      // tours: tours,
     },
   });
 };
@@ -45,74 +27,34 @@ exports.getTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: tours[req.requestedTourIndex],
+      // tour: tours[req.requestedTourIndex],
     },
   });
 };
 
-exports.createTour = (req, res) => {
-  const newTourId = tours[tours.length - 1].id + 1;
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  const newTour = { id: newTourId, ...req.body };
-  tours.push(newTour);
-  fs.writeFile(DB_PATH, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'Internal Server Error',
-        message:
-          'Some error was occured when writeing data to the database, pleace try to rewrite data',
-        data: {},
-      });
-    } else {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
+exports.createTour = async (req, res) => {
+  const newTour = await Tour.create({});
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      // tour: newTour,
+    },
   });
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  const updatedTour = { id, ...req.body };
-  tours.splice(req.requestedTourIndex, 1, updatedTour);
-  fs.writeFile(DB_PATH, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'Internal Server Error',
-        message:
-          'Some error was occured when writeing data to the database, pleace try to update data',
-        data: {},
-      });
-    } else {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour: updatedTour,
-        },
-      });
-    }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      // tour: updatedTour,
+    },
   });
 };
 
 exports.deleteTour = (req, res) => {
-  tours.splice(req.requestedTourIndex, 1);
-  fs.writeFile(DB_PATH, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'Internal Server Error',
-        message:
-          'Some error was occured when writeing data to the database, pleace try to delete data',
-        data: {},
-      });
-    } else {
-      res.status(204).json({
-        status: 'No Content',
-        data: null,
-      });
-    }
+  res.status(204).json({
+    status: 'No Content',
+    data: null,
   });
 };
