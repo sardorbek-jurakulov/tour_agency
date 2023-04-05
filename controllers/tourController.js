837a87a7ed00d9c4db554e1cbd7ec08666967ed1
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-05 11:43:57 +0500

    get all tours and get tours functionalities has been added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,24 +12,42 @@ exports.checkBody = (req, res, next) => {
 };
 
 // route handlers
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    data: {
-      // tours: tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      // results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Not found',
+      message: 'Bad request',
+      data: {},
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      // tour: tours[req.requestedTourIndex],
-    },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Failed',
+      message: 'Tour not found',
+      data: {},
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
