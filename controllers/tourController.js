a54d47b36ac724a0e9cbbb233e010a2c8a3f21a0
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-06 11:46:26 +0500

    data filter functionality has been added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,11 +3,27 @@ const Tour = require('../models/tourModel');
 // route handlers
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
-      // results: tours.length,
+      results: tours.length,
       data: {
         tours,
       },
