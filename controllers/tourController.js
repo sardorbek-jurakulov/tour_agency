f82c6b10de86b5cd2f217e1003ee49cdbd5dffdb
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-10 12:28:40 +0500

    monthly statistics functionality has been complated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -208,6 +208,27 @@ exports.getMonthlyPlan = async (req, res) => {
           },
         },
       },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 2,
+      },
     ]);
 
     res.status(200).json({
