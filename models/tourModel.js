e2ad07a0a7757bdb5422bd46de816275b6673b1d
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-10 15:49:32 +0500

    some query middlewares has been added

--- models/tourModel.js
+++ models/tourModel.js
@@ -60,6 +60,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -82,6 +86,19 @@ tourSchema.pre('save', function (next) {
 // eslint-disable-next-line prefer-arrow-callback
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
+//   next();
+// });
+
+//  QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  // this.start = Date.now();
+  next();
+});
+
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+//   // console.log(docs);
 //   next();
 // });
 
