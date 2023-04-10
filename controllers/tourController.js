dc56926b1bd9a2c0bda532b5e4f9fe6f33196d4f
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-10 11:54:24 +0500

    yearli statistics has been added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -192,3 +192,34 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Not found',
+      message: 'Requested tour not found',
+    });
+  }
+};
