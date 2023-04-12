7da16d14c14c50471a9b4dea354f2f68a3c29e15
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-12 14:09:28 +0500

    error handler has been added in .findByIDAndUpdate() method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -114,6 +115,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
   //   });
   // }
   const tour = await Tour.findById(req.params.id);
+  if (!tour) {
+    return next(new AppError(`No tour found with ${req.params.id} ID`, 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
