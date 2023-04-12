8f0ce1294420b49915eb19104235c00fed4e4621
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-12 14:45:56 +0500

    WIP

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,27 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(`Error has been occured at ${err.stack}`);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   err.message = err.message || 'Oops, some error has been accured';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev();
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd();
+  }
 };
