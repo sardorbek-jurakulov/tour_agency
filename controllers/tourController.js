4ce450864fb8cd522794a6ad627760b91452accf
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-12 12:15:24 +0500

    catching error in async function has been added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -100,22 +100,19 @@ exports.getTour = async (req, res) => {
   }
 };
 
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: err,
-    });
-  }
+const catchAsync = (fn) => (req, res, next) => {
+  fn(req, res, next).catch(next);
 };
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
+});
 
 exports.updateTour = async (req, res) => {
   try {
