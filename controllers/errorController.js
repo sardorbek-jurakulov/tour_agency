07f434fb7f6674f599031f1c9bc58c08ffaeb039
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-12 15:53:33 +0500

    WIP

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,20 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+  // Programming or other  unknown error: don't leak error details
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
