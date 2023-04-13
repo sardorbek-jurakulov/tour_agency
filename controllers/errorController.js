e4c31aea4298d136b7c9290d1cabb8ece1e1e4af
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-13 12:56:12 +0500

    dublicate name error validation has been added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  // console.log(value);
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -45,6 +52,9 @@ module.exports = (err, req, res, next) => {
     let error = JSON.parse(JSON.stringify(err));
     if (error.name === 'CastError') {
       error = handleCastErrorDB(error);
+    }
+    if (error.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
     }
     sendErrorProd(error, res);
   }
