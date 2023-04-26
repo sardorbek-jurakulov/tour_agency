a4c56d8a3daddebe4f7504f2a9fd6db92d235812
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-26 15:50:58 +0500

    WIP

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,6 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-
   res.status(statusCode).json({
     status: 'success',
     token,
