e805e717c55f7a8ca802bb7fff28717df03d32eb
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-26 09:43:24 +0500

    WIP

--- controllers/authController.js
+++ controllers/authController.js
@@ -139,5 +139,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 })
 
 exports.resetPassword = (req, res, next) => {
-  
+  // 1) Get user based on the token
+
+  // 2) If token has not expired, and there is user, set the new password
+
+  // 3) Update changedPasswordAt property for the (current) user
+
+  // 4) Log the user in, send JWT (to client)
 }
