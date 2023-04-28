b9f27fba7562270b38f6f1590561a1aef8122887
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-04-28 16:11:22 +0500

    WIP

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// quyidagi statement bitta IP'ga 1 soatda 100 ta so'rovga ruxsat beradi
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
