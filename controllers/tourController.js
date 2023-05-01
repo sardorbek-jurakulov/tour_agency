578f4fe518f471c8ea0c90d32fedcc41fd60f481
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-01 13:27:15 +0500

    WIP

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -114,10 +114,13 @@ exports.getTour = catchAsync(async (req, res, next) => {
   //     message: 'Tour not found',
   //   });
   // }
-  const tour = await Tour.findById(req.params.id).populate({
-    path: 'guides',
-    select: '-_v -passwordChangedAt',
-  });
+
+  // const tour = await Tour.findById(req.params.id).populate({
+  //   path: 'guides',
+  //   select: '-__v -passwordChangedAt',
+  // });
+
+  const tour = await Tour.findById(req.params.id);
 
   if (!tour) {
     return next(new AppError(`No tour found with ${req.params.id} ID`, 404));
--- controllers/userController.js
+++ controllers/userController.js
@@ -60,7 +60,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, {
-    active: true,
+    active: false,
   });
 
   res.status(204).json({
