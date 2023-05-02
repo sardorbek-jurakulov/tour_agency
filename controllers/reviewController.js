81edce0e224246eefb078db87dbb745702580c07
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-02 10:43:15 +0500

    WIP

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -37,6 +37,13 @@ exports.createReview = catchAsync(async (req, res, next) => {
   // const userReviewId = req.body.user;
   // const tourReviewId = req.body.tour;
 
+  // Allow nested routes
+  if (!req.body.tour) {
+    req.body.tour = req.params.tourId;
+  }
+  if (!req.body.user) {
+    req.body.user = req.user.id;
+  }
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: 'success',
