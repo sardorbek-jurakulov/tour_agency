72bb8dd7421e9dc18bc879278c9c114f50b35fe0
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-03 09:39:38 +0500

    WIP

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
 // const reviewController = require('../controllers/reviewController');
-const reviewRouter = require("./reviewRoutes");
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
