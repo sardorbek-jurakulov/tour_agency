d2bde6b08523dcbf104d02c618b1d8c7a57d51fa
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-03 09:22:28 +0500

    WIP

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+// POST /tours/234fad4/reviews
+// POST /reviews
 
 router
   .route('/')
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+// const reviewController = require('../controllers/reviewController');
+const reviewRouter = require("./reviewRoutes");
 
 const router = express.Router();
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -25,14 +36,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
