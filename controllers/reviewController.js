6067eb0763fddfe2d6330504a75a2dd5f5fad3d1
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-04 10:41:43 +0500

    WIP

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -47,15 +47,6 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 }
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  });
-});
-
+exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
