890c2ca339e2197c59d7a1a4837c824fc95786ee
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-04 11:02:36 +0500

    WIP

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -40,3 +40,23 @@ exports.createOne = (Model) =>
       },
     });
   });
+
+exports.getOne = (Model, popOptions) => 
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) {
+      query = query.populate(popOptions);
+    }
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError(`No tour found with ${req.params.id} ID`, 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        [Model.modelName.toLowerCase()]: doc,
+      },
+    });
+  });
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res, next) => {
+// quyidagi factory.getOne() methodining ikkinchi parametrida berilgan populateni man tour modelida ham get all tour uchun ham get one tour uchun ishlaydigan qilib pre middleware'ida yozib qo'yganman, kodda kanflikt bo'lmasligi uchun bu yerga ham yozib qo'ydim.
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
+catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
   if (!tour) {
