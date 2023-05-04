8b0e97a2b231ef6d9bb1cde5b7dd597200c8bdb7
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-04 15:03:17 +0500

    WIP

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,24 +6,17 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Quyidagi koddan keyingi barcha route lar uchun authControllerning protect middleware'ini ishlat deb buyuruq berdik.
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')
