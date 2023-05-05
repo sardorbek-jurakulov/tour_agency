3b91e6fc56de9660c45113558989e3b7ee3f7720
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-05 08:33:24 +0500

    WIP

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,10 +10,10 @@ router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Quyidagi koddan keyingi barcha route lar uchun authControllerning protect middleware'ini ishlat deb buyuruq berdik.
+// Protect all routes after this middleware
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
-
 router.get('/me', userController.getMe, userController.getUser);
 router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
