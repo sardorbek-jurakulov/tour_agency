3732b9e2eb8bd8e3ef47c474289dd0f5d6079f33
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-07 01:57:55 +0500

    WIP

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,7 +34,7 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
-reviewSchema.index({ tour: 1, user: 1 });
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
   // Bu mening tahminiy yechimim, u Jonasni kodi bilan bir xil natija qaytaryapti
