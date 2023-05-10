4555bac360884986e84f97fb5965e8f695582d4f
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-10 15:03:18 +0500

    WIP

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -22,7 +22,7 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
-    title: `${tour.name} tour`,
+    title: `${tour.name} Tour`,
     tour,
   });
 });
