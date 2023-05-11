ec68ee90633750b3edea5dbd2f4ed51cb5bcacee
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-11 13:24:42 +0500

    WIP

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,16 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
+  // center: [-118.11, 34.11],
+  // zoom: 10,
+  // interactive: false
+});
+
+const bounds = new mapboxgl.LatLngBounds();
+
