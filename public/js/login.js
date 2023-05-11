e75c0444ff18a7f1eb2bffbc32da7bbf7fa81f8f
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-11 23:30:59 +0500

    WIP

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
-document.querySelector('.form').addEventListener('submit', e => {
+document.querySelector('.form').addEventListener('submit', (e) => {
   e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 });
