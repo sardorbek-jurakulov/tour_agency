a75927166a70a4b82f6d82865d6229ae026ff912
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-12 11:33:56 +0500

    WIP

--- public/js/login.js
+++ public/js/login.js
@@ -10,10 +10,15 @@ const login = async (email, password) => {
         password
       }
     });
-    console.log(res);
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err?.response?.data);
-    console.log('Xatolikka kirdi');
+    alert(err?.response?.data?.message);
   }
 };
 
