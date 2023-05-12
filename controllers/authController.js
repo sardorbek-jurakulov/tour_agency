3dca08d3175ff2e5b6e5b698003994cf80e1e946
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-12 10:15:26 +0500

    WIP

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,6 +73,30 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  // 1) verify token
+  if (req.cookies.jwt) {  
+    const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+
+    // 2) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // 3) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+    
+    // THERE IS A LOGGED IN USER
+    res.locals.user = currentUser;
+    next();
+  }
+  next();
+});
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
