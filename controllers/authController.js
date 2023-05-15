69a82ea6853315ce90de2db898e5cfb611ce7441
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-15 15:29:44 +0500

    WIP

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,6 +78,13 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+}
+
 // Only for rendered pages, no errors!
 exports.isLoggedIn = catchAsync(async (req, res, next) => {
   // 1) verify token
