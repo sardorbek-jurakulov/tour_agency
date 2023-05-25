86cfa47c757c25c55ceab10764f3dbaa6945e566
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-25 15:10:36 +0500

    WIP

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
-const Bookings = require('../models/bookingModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking') {
+    res.locals.alert =
+      'Your booking was successful! Please check your email for a confirmation. If your booking doesn\'t show up here immediately, please come back later.';
+  }
+  next();
+};
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from collection
--- public/js/index.js
+++ public/js/index.js
@@ -71,3 +71,9 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+
+if (alertMessage) {
+  showAlert('success', alertMessage, 20);
+}
